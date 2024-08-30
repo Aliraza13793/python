488228eba7eea2c711961ad124174fe8b9d5034b
aliraza13793 <aliraza13793@gmail.com>
2024-08-31 02:38:49 +0500

    ok

---
+++ class.py
@@ -0,0 +1,122 @@
+name : str = "Ali Raza"
+print(name)
+print(type(name))
+print(id(name))
+print("hello World")
+name_t : str = "yghghghaHG"
+print(name_t.upper())
+print(name_t.lower())
+print(name_t.capitalize())
+
+# capitalize first letter of each word
+
+name = "Alice"
+age = 30
+greeting = f"Hello, my name is {name} and I am {age} years old."
+print(greeting)
+
+# f string
+
+# + - * // % / ** 
+# Python Arithmetic Operators
+
+# Python Comparison Operators
+#Python Assignment Operators
+
+#Python Logical Operators
+ # and or not
+
+ #list or tuples
+ # A list of integers
+numbers = [1, 2, 3, 4, 5]
+
+# A list of strings
+fruits = ["apple", "banana", "cherry"]
+
+# A list of mixed data types
+mixed_list = [1, "hello", 3.14, True]
+
+
+# A tuple of integers
+numbers = (1, 2, 3, 4, 5)
+
+# A tuple of strings
+fruits = ("apple", "banana", "cherry")
+
+# A tuple of mixed data types
+mixed_tuple = (1, "hello", 3.14, True)
+# Accessing the first element
+first_fruit = fruits[0]  # 'apple'
+
+# Accessing the last element
+last_fruit = fruits[-1]  # 'cherry'
+
+# Slicing a tuple
+some_fruits = fruits[1:3]  # ('banana', 'cherry')
+
+# for loop
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# Example: Counting down from 5 to 1
+count = 5
+while count > 0:
+    print(count)
+    count -= 1
+
+
+
+
+print(name.replace("Ali", "Yasir"))
+
+# replace a word    
+
+print(name.split(" "))  
+  
+# split a string into a list of words
+
+print(len(name))
+
+# length of a string    
+
+print(name.startswith("A"))
+
+# check if a string starts with a particular character
+
+print(name.endswith("z"))
+
+# check if a string ends with a particular character
+
+print(name.count("i"))
+
+# count the number of particular character in a string
+
+print(name.index("i"))
+
+# find the index of a particular character in a string
+
+print(name.find("i"))
+
+# find the index of a particular character in a string
+
+print(name.isalpha())
+
+# check if a string contains only alphabets
+
+print(name.isdigit())
+
+# check if a string contains only digits
+
+print(name.isalnum())
+
+# check if a string contains only alphabets and digits
+
+print(name.islower())
+
+# check if a string contains only lowercase letters
+namee : str = "Hello"
+print("\t", namee)
+
+# print a string with a tab in between each characterpython 
+
